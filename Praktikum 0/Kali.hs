b53590105effc53e0bf2bc435a704af1382bc293
ilhamsyahids <ilhamsyahids@gmail.com>
2019-02-05 21:35:46 +0700

    Praktikum 0

---
+++ Praktikum 0/Kali.hs
@@ -0,0 +1,19 @@
+-- NIM/Nama  	: 16518360/Ilham Syahid S
+-- Nama File	: Kali.hs
+-- Topik	: Ekspresi Dasar
+-- Tanggal 	: 23 Januari 2019
+-- Deskripsi	: Perkalian 2 bilangan integer
+
+module Kali where
+
+-- Perkalian 2 bilangan
+-- Definisi dan Spesifikasi
+kali :: Int -> Int -> Int
+	-- kali a b menghasilkan a*b
+	-- sebuah bilangan integer
+
+-- Realisasi
+kali a b = a * b
+
+-- Contoh aplikasi
+-- kali 2 4
---
+++ Praktikum 0/Pangkat2.hs
@@ -0,0 +1,19 @@
+-- NIM/Nama  	: 16518360/Ilham Syahid S
+-- Nama File	: Pangkat2.hs
+-- Topik	: Ekspresi Dasar
+-- Tanggal 	: 23 Januari 2019
+-- Deskripsi	: Pangkat 2 bilangan integer
+
+module Pangkat2 where
+
+-- Pangkat2 - pangkat2(x)
+-- Definisi dan Spesifikasi
+pangkat2 :: Int -> Int
+	-- fx2(x) menghitung pangkat dua dari x
+	-- sebuah bilangan integer
+
+-- Realisasi
+pangkat2 x = x^2
+
+-- Contoh aplikasi
+-- pangkat2 2
